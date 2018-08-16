d13a17c5900a9d28af0cb16206cdb4afb85019ee
Spyion <m4tm4t380@gmail.com>
2018-08-16 19:42:06 +0200

    centerBottom

--- Assets/Scripts/ActiveSprite.cs
+++ Assets/Scripts/ActiveSprite.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class ActiveSprite : MonoBehaviour
 {
-
+    [SerializeField]
+    bool centerBottom;
     SpriteRenderer spriteRenderer;
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        if (centerBottom)
+            transform.position = new Vector3(0, spriteRenderer.sprite.texture.height / 4f / spriteRenderer.sprite.pixelsPerUnit, 0);
     }
     void Update()
     {
